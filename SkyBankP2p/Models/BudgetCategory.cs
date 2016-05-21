8768118ecdf88d609f162f0df3b51a2f5edad0f7
ateregulov <sliceice@gmail.com>
2016-05-21 16:38:28 +0300

    BudgetCategory class

--- SkyBankP2p/Models/BudgetCategory.cs
+++ SkyBankP2p/Models/BudgetCategory.cs
@@ -7,5 +7,9 @@ namespace SkyBankP2p.Models
 {
     public class BudgetCategory
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsParent { get; set; }
+        public bool IsPositive { get; set; }
     }
 }
