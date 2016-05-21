768b08be1e8eef20e9d3fe4db81bb3b2e2cbda01
ateregulov <sliceice@gmail.com>
2016-05-21 14:57:46 +0300

    empty asp.net web-application

---
+++ SkyBankP2p/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SkyBankP2p.MvcApplication" Language="C#" %>
---
+++ SkyBankP2p/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SkyBankP2p.Startup))]
+namespace SkyBankP2p
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
