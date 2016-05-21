2565ba999fbdaa5cde75464a202894b2683758f1
ateregulov <sliceice@gmail.com>
2016-05-21 16:46:47 +0300

    BudgetLimit class

---
+++ SkyBankP2p/Controllers/BalanceController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SkyBankP2p.Controllers
+{
+    public class BalanceController : Controller
+    {
+        // GET: Balance
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- SkyBankP2p/Models/BudgetLimit.cs
+++ SkyBankP2p/Models/BudgetLimit.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,11 @@ namespace SkyBankP2p.Models
 {
     public class BudgetLimit
     {
+        public int Id { get; set; }
+        public int BudgetCategoryId { get; set; }
+        public BudgetCategory BudgetCategory { get; set; }
+        public decimal Amount { get; set; }
+        [Range(1, 12)]
+        public int Month { get; set; }
     }
 }
