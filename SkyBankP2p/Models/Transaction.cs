4b30f166aa50a915a1c16da055f1c9069dac876f
ateregulov <sliceice@gmail.com>
2016-05-21 16:56:13 +0300

    Transaction class

--- SkyBankP2p/Models/Transaction.cs
+++ SkyBankP2p/Models/Transaction.cs
@@ -7,5 +7,15 @@ namespace SkyBankP2p.Models
 {
     public class Transaction
     {
+        public int Id { get; set; }
+        public DateTime DateTime { get; set; }
+        public decimal Amount { get; set; }
+        public int BudgetCategoryId { get; set; }
+        public BudgetCategory BudgetCategory { get; set; }
+        public int OperationTypeId { get; set; }
+        public OperationType OperationType { get; set; }
+        public int WalletId { get; set; }
+        public Wallet Wallet { get; set; }
+        public string Comment { get; set; }
     }
 }
