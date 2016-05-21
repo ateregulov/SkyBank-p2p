112624659c5befcbd22fba1629e93b8f939f7e81
ateregulov <sliceice@gmail.com>
2016-05-21 16:57:17 +0300

    CurrentStateViewModel

---
+++ SkyBankP2p/Models/CurrentStateViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SkyBankP2p.Models
+{
+    public class CurrentStateViewModel
+    {
+        public List<Item> Items { get; set; }
+
+        public class Item
+        {
+            public string BudgetCategory { get; set; }
+            public decimal FactAmount { get; set; }
+            public decimal PlanAmount { get; set; }
+            public decimal Difference
+            {
+                get
+                {
+                    return PlanAmount - FactAmount;
+                }
+                set { }
+            }
+
+        }
+    }
+}
